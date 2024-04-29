12332c43a078aca21cff5b3cd6a4f40fca09f435
eucarizan <eucarizan@gmail.com>
2024-04-30 01:20:45 +0800

    added creation of other setup files

--- 00_init/setup.sh
+++ 00_init/setup.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 echo -e "set relativenumber\nset number\nset shiftwidth=2\nset autoindent" > ~/.vimrc
 
+# add check if exists
+echo '#!/bin/bash' > append_task.sh 
+echo 'tail -n1 /workspace/.bash_history >> task.sh' >> append_task.sh
+echo -e "if [[ \$1 ]]; then\n  sed -ri \"\$s/(.*)/\1 \$1/\" task.sh\nfi" >> append_task.sh
+chmod +x append_task.sh
+
+echo '#!/bin/bash' > delete_last_line.sh 
+echo "sed -i '$ d' task.sh" >> delete_last_line.sh
+chmod +x delete_last_line.sh
+
+echo '#!/bin/bash' > task.sh 
+
+echo -e "\nalias dbike=\"sed -i '$ d' bike-shop.sh\"" >> ~/.bashrc
+echo "alias adtask=\"./append_task.sh\"" >> ~/.bashrc
+
+export PSQL="psql -U freecodecamp -d bikes -c"
